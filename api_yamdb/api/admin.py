6a1e383c5465f5b5fef9ecc3738302ab476b35ce
Бернова Анна <bast33@yandex.ru>
2022-07-30 16:36:28 +0300

    register usermodel

--- api_yamdb/api/admin.py
+++ api_yamdb/api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'email',
+        'role',
+        'bio',
+        'first_name',
+        'last_name',
+    )
+    search_fields = ('username', 'role',)
+    list_filter = ('username',)
