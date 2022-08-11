75650058fbe35e365632ea58875fb8dbae623b0c
Бернова Анна <bast33@yandex.ru>
2022-08-11 16:00:12 +0300

    fix 1 review

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,16 +3,12 @@ from rest_framework import permissions
 
 class AdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (
-            request.user.is_admin
-            or request.user.is_staff
-        )
+        return request.user.is_authenticated and (
+            request.user.is_admin)
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.user.is_admin
-            or request.user.is_staff
-        )
+        return request.user.is_authenticated and (
+            request.user.is_admin)
 
 
 class ModeratorPermission(permissions.BasePermission):
@@ -20,14 +16,12 @@ class ModeratorPermission(permissions.BasePermission):
         user = request.user
         return (
             user.is_authenticated and user.is_moderator
-            or user.is_staff
         )
 
     def has_object_permission(self, request, view, obj):
         user = request.user
         return (
             user.is_authenticated and user.is_moderator
-            or user.is_staff
         )
 
 
